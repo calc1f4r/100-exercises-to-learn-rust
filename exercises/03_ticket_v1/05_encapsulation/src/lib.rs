190f9ddd2f13180715566efb518657e62d8e59fc
calc1r4r <20yashs09@gmail.com>
2024-11-25 14:49:46 +0530

    03/04

--- exercises/03_ticket_v1/05_encapsulation/src/lib.rs
+++ exercises/03_ticket_v1/05_encapsulation/src/lib.rs
@@ -4,7 +4,6 @@ pub mod ticket {
         description: String,
         status: String,
     }
-
     impl Ticket {
         pub fn new(title: String, description: String, status: String) -> Ticket {
             if title.is_empty() {
@@ -30,10 +29,15 @@ pub mod ticket {
             }
         }
 
-        // TODO: Add three public methods to the `Ticket` struct:
-        //  - `title` that returns the `title` field.
-        //  - `description` that returns the `description` field.
-        //  - `status` that returns the `status` field.
+        pub fn title(self)-> String{
+            self.title
+        }
+        pub fn description(self)->String{
+            self.description
+        }
+        pub fn status(self)->String{
+            self.status
+        }
     }
 }
 
